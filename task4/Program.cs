3af75cd1e9b511d9a2fd2104f16f00634c63fb30
Ellina <elyushaelya@gmail.com>
2023-02-16 02:23:51 +0700

    сдача дз семинар1

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные 
+//числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+int remainder;
+
+while (current < N)
+{
+    remainder = current % 2;
+    if (remainder == 0)
+    {
+        Console.WriteLine($"{current}");
+    }
+    current++;
+}
